458f685f39f9f36ad35016ef784a70b805071979
Samir <smrcheese@gmail.com>
2023-10-08 18:46:09 +0100

    added and fixed

--- ft_split.c
+++ ft_split.c
@@ -6,57 +6,59 @@
 /*   By: sezequie <sezequie@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 18:17:32 by sezequie          #+#    #+#             */
-/*   Updated: 2023/10/07 02:25:43 by sezequie         ###   ########.fr       */
+/*   Updated: 2023/10/08 18:29:06 by sezequie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	count_words(const char *str, char c)
+static	int	splitcount(const char *check, char c)
 {
-	size_t	i;
-	size_t	j;
+	int	bo;
+	int	k;
+	int	l;
 
-	i = 0;
-	j = 0;
-	while (*str != '\0')
+	bo = 0;
+	k = 0;
+	l = 0;
+	while (check && check[k] != '\0')
 	{
-		if (*str != c && j == 0)
+		if (bo == 0 && check[k] != c)
 		{
-			j = 1;
-			i++;
+			bo = 1;
+			l++;
 		}
-		else if (*str == c)
-			j = 0;
-		str++;
+		else if (check[k] == c)
+			bo = 0;
+		k++;
 	}
-	return (i);
+	return (l);
 }
 
 char	**ft_split(char const *s, char c)
 {
+	char	**splitted;
 	size_t	i;
-	size_t	j;
-	int		index;
-	char	**final;
+	int		o;
+	size_t	p;
 
-	final = (char **)malloc((count_words(s, c) + 1) * sizeof(char *));
-	if (final == NULL)
-		return (NULL);
+	o = -1;
 	i = 0;
-	j = 0;
-	index = -1;
+	p = 0;
+	splitted = malloc((splitcount(s, c) + 1) * sizeof(char *));
+	if (!splitted || !s)
+		return (0);
 	while (i <= ft_strlen(s))
 	{
-		if (s[i] != c && index < 0)
-			index = i;
-		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
+		if (s[i] != c && o < 0)
+			o = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && o >= 0)
 		{
-			final[j++] = ft_substr(s, index, i);
-			index = -1;
+			splitted[p++] = ft_substr(s, o, i - o);
+			o = -1;
 		}
 		i++;
 	}
-	final[j] = 0;
-	return (final);
+	splitted[p] = 0;
+	return (splitted);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sezequie <sezequie@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 22:35:01 by sezequie          #+#    #+#             */
-/*   Updated: 2023/10/07 02:27:14 by sezequie         ###   ########.fr       */
+/*   Updated: 2023/10/08 18:37:25 by sezequie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,5 +49,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
